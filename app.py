a5a9debff126ff7bb8d3298f8546e9674b69adcc
Rasheeda-sa <rsheeda2018@gmail.com>
2025-06-12 13:16:20 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure and GitHub!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(debug=True)
